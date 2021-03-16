8f96715ec1274bba60f02ce8bcdb60d49139db91
Suyesh Amatya <hisuyesh@hotmail.com>
2021-03-16 17:44:39 +0100

    Search query build start

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,18 +9,29 @@ class SearchController extends Controller
 {
     //
     public function search(Request $request){
-        if ($request->has("model") and isset($request->model)){
+        //dd($request);
+        
+        $columns = array();
+        $results = User::select('first_name', 'last_name', 'pnr');
+        dd($results);
+        
+        if($request->has("model") and isset($request->model)){
+            $results = User::when($request->get('orders'), function ($query) {
+                $query->whereHas('orders');
+            })->get(['first_name','status']);
+            dd($results);
            // dump(User::whereRaw('NOT FIND_IN_SET(?, roles)', [config('constants.roles.ADMIN_ROLE')])
            //     ->with('orders')->get()->toJson());
            
-            return response()->json(array('data'=> User::get(["first_name", "last_name"]),  'columns' =>['First Name','Last Name']) );
+            //return response()->json(array('data'=> User::get(["first_name", "last_name"]),  'columns' =>['First Name','Last Name']) );
             
             //return response->json('data', User::get("first_name")->toJson())
             //->with('columns', 'first_name');
             
             
         }
-            
+        
+            return;
         
     }
 }
--- resources/views/admin/reports.blade.php
+++ resources/views/admin/reports.blade.php
@@ -5,7 +5,7 @@
         <h1 class="h2">Generate Report</h1>
 </div>
 <div class="card d-flex pt-3 pb-2 mb-3">
-	<form method="post" class="form-inline" action="{{action('SearchController@search')}}">
+	<form method="post" class="form-inline" action="{{action('SearchController@search')}}" id="searchForm">
 		@csrf
 		<div class="row ml-3">
 		
@@ -13,20 +13,44 @@
 		<select class="custom-select my-1 mr-sm-2" name="model" id="model">
 			<option value="" selected>Select...</option>
 			<option value="User">Users</option>
-			<option value="2">Two</option>
-			<option value="3">Three</option>
 		</select>
 		</div>
 
 		<div class="col">
-			<input type="checkbox" class="custom-control-input"
-				id="customControlInline"> <label class="custom-control-label"
-				for="customControlInline">Remember my preference</label>
+			<label for="pnr" class="sr-only">PNR</label>
+            <input type="text" class="form-control my-1 mr-sm-2" name="pnr" value="{{old('pnr')}}" placeholder="ÅÅÅÅMMDDNNNN"/>
+   		</div>
+		
+		<div class="col">
+		<select class="custom-select my-1 mr-sm-2" name="filter_criteria" id="filter_criteria">
+			<option value="" selected>Select...</option>
+			<option value="orders">Orders</option>
+			<option value="without_orders">Without Orders</option>
+			<option value="kits">Kits</option>
+			<option value="kits_dispatched">Kits Dispatched</option>
+			<option value="samples_received">Kits/Samples Received</option>
+			<option value="samples">Samples</option>
+			<option value="results_reported">Results Reported</option>
+		</select>
 		</div>
 		
+		<div class="col">
+            <label for="from_date" class="sr-only">From Date</label>
+            <input class="datepicker form-control my-1 mr-sm-2" name="from_date" data-date-format="yyyy-mm-dd" 
+            value="{{old('from_date')}}" placeholder="From">
+        </div>
+        
+        <div class="col">
+            <label for="to_date" class="sr-only">To Date</label>
+            <input class="datepicker form-control my-1 mr-sm-2" name="to_date" data-date-format="yyyy-mm-dd" 
+            value="{{old('to_date')}}" placeholder="To">
+        </div>
+		
 		</div>
-		<div class="row">
-		<button type="submit" class="btn btn-primary my-1">Filter</button>
+		<div class="row ml-3">
+		<div class="col">
+			<button type="submit" class="btn btn-primary my-1">Search</button>
+		</div>
 		</div>
 	</form>
 	
@@ -46,43 +70,108 @@
 <script type="text/javascript">
 
     $(document).ready(function() {
-    	$('form').submit(function(event){
-        	event.preventDefault();
+    	$('input[name=pnr]').hide();
+    	$('#filter_criteria').hide();
+    	$('input[name=from_date]').hide();
+		$('input[name=to_date]').hide();
+    	
+    	$('#model').on('change', function() {
+    		if(this.value=="User"){
+    			$('input[name=pnr]').show();
+    			$('#filter_criteria').show();
+    			$('input[name=from_date]').show();
+    			$('input[name=to_date]').show();
+        	}
+    		else{
+    			$('input[name=pnr]').hide();
+    			$('#filter_criteria').hide();
+    			$('input[name=from_date]').hide();
+    			$('input[name=to_date]').hide();
+        	}
+    	});
+        
+    	$('#searchForm').submit(function(event){
+    		event.preventDefault();
+    		
+        	
         	var formData = {
                 	'_token'			: $('input[name=_token]').val(),
-                    'model'              : $('select[name=model]').val(),
-                    //'email'             : $('input[name=email]').val(),
-                    //'superheroAlias'    : $('input[name=superheroAlias]').val()
+                    'model'             : $('select[name=model]').val(),
+                    'pnr'				: $('input[name=pnr]').val(),
+                    'filter_criteria'   : $('select[name=filter_criteria]').val(),
+                    'from_date'         : $('input[name=from_date]').val(),
+                    'to_date'    		: $('input[name=to_date]').val()
             };
         	
         	$.ajax({
                 type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
                 url         : "{{action('SearchController@search')}}", // the url where we want to POST
                 data        : formData, // our data object
-                dataType    : 'json', // what type of data do we expect back from the server
                 success:function(response){
-					var columns = [];
-					var columnNames = Object.keys(response.data[0]);
-					let i = 0;
-					$.each(columnNames, function( key, value ) {
-						
-						columns.push({data : columnNames[key], title: response.columns[i]})
-						i++;
-					});
-					/*$.each(response.columns, function(key, value){
-						columns.push({title: value})
-					});*/
-					$('#results_table').DataTable({
-						data: response.data,
-						columns: columns
+                    if(typeof response == 'object'){
+    					var columns = [];
+    					var columnNames = Object.keys(response.data[0]);
+    					let i = 0;
+    					$.each(columnNames, function( key, value ) {
+    						columns.push({data : columnNames[key], title: response.columns[i]})
+    						i++;
+    					});
 
-                    });
-					
+    					if ( $.fn.dataTable.isDataTable( '#results_table' ) ) {
+    					    $('#results_table').DataTable().destroy();
+    					    $('#results_table').empty();
+    					}
+    					$('#results_table').DataTable({
+    						data: response.data,
+    						columns: columns,
+    						dom: 'Blfrtip',
+    			            buttons: [
+    			                'colvis', 
+    			                {
+    			                	extend: 'copy',
+    			                	title: 'Query Results Export',
+    			                },
+    			                {
+    			                	extend: 'csv',
+    			                	title: 'Query Results Export',
+    			                },
+    		                 	{
+    		                 		extend: 'excel',
+    		                 		title: 'Query Results Export',
+    		                  	},
+    		                  	{
+    		                  		extend: 'pdf',
+    		                  		title: 'Query Results Export',
+    		                   	},
+    		                   	{
+    		                   		extend: 'print',
+    		                   		title: 'Query Results Export',
+    		                    }
+    			            ],
+    			            
+
+                        });
+    					
+                    }
+                    else{
+                    	if ( $.fn.dataTable.isDataTable( '#results_table' ) ) {
+    					    $('#results_table').DataTable().destroy();
+    					    $('#results_table').empty();
+    					}
+                    }
                 }
-            })
+            
+
+            });
         });
         	
         
+    });
+
+    $('.datepicker').datepicker({
+    	dateFormat: "yy-mm-dd",
+    	showWeek: true,
+    	firstDay: 1
     });
 
 </script>
